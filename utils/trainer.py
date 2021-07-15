2de3cb8c80892bf45f86e3f24b74d65d5a68eca7
Anastasiia-Selezen <49845205+Anastasiia-Selezen@users.noreply.github.com>
2021-07-15 17:04:51 +0300

    Update trainer.py

--- utils/trainer.py
+++ utils/trainer.py
@@ -4,7 +4,7 @@ from sklearn.svm import SVC
 class Estimator:
     @staticmethod
     def fit(train_x, train_y):
-        return SVC().fit(train_x, train_y)
+        return SVC().fit(train_x, train_y, class_weight="balance")
 
     @staticmethod
     def predict(trained, test_x):
